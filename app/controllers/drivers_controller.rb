efd1b16e15f3b80e55612610352380c33793cd77
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-07-25 23:13:20 +0300

    controllers updated

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,45 @@
 class DriversController < ApplicationController
-end
+    before_action :set_driver, only: [:show, :update, :destroy]
+  
+    def index
+      @drivers = Driver.all
+      render json: @drivers
+    end
+  
+    def show
+      render json: @driver
+    end
+  
+    def create
+      @driver = Driver.new(driver_params)
+  
+      if @driver.save
+        render json: @driver, status: :created
+      else
+        render json: @driver.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @driver.update(driver_params)
+        render json: @driver
+      else
+        render json: @driver.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @driver.destroy
+    end
+  
+    private
+  
+    def set_driver
+      @driver = Driver.find(params[:id])
+    end
+  
+    def driver_params
+      params.require(:driver).permit(:name, :email, :password)
+    end
+  end
+  
