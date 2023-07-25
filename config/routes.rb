1817bf5f5f39b3d574421a7cb7b3eec55a404140
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-07-25 23:16:03 +0300

    routes ghenerated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Routes for BusesController
+  resources :buses do
+    collection do
+      get 'search_by_route', to: 'buses#search_by_route'
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Routes for DriversController
+  resources :drivers
+
+  # Routes for CustomersController
+  resources :customers
+
+  # Routes for BookingsController
+  resources :bookings
+
+  # Additional routes can be added here for other custom functionality.
+
+  # Root route (optional)
+  # root 'buses#index'
 end
