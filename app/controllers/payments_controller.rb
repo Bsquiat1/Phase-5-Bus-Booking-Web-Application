660348d3662b224e88051da7befb567d7dc9cb0d
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-08-09 11:25:06 +0300

    started mpesa configuration

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,12 @@
+# app/controllers/payments_controller.rb
+
+class PaymentsController < ApplicationController
+    def online_payment_callback
+      # Handle online payment callback
+    end
+  
+    def ussd_payment_callback
+      # Handle USSD payment callback
+    end
+  end
+  
---
+++ app/services/mpesa_service.rb
@@ -0,0 +1,14 @@
+# app/services/mpesa_service.rb
+
+class MpesaService
+    def self.get_access_token
+      # Code for obtaining the access token
+    end
+    def self.initiate_payment(amount, phone_number, paybill_number, callback_url)
+        # Code for initiating the payment
+      end
+      def self.initiate_ussd_payment(amount, customer_phone_number, paybill_number, timeout_callback_url, result_callback_url)
+        # Code for initiating the USSD payment
+      end
+  end
+  
