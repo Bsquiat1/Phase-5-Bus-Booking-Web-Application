a761e23c4f639ba9b2275008d8e9f8d96b364310
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-08-09 11:35:49 +0300

    started mpesa configuration

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,12 +1,15 @@
-# app/controllers/payments_controller.rb
-
 class PaymentsController < ApplicationController
-    def online_payment_callback
-      # Handle online payment callback
+    def initiate_online_payment
+      amount = params[:amount]
+      phone_number = params[:phone_number]
+      paybill_number = 'YOUR_PAYBILL_NUMBER'
+      callback_url = 'YOUR_CALLBACK_URL'
+  
+      response = MpesaService.initiate_payment(amount, phone_number, paybill_number, callback_url)
+  
+      # Handle the response, redirect to success/failure page, etc.
     end
   
-    def ussd_payment_callback
-      # Handle USSD payment callback
-    end
+    # Implement USSD payment callback handler here
   end
   
